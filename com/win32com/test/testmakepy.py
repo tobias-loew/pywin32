14efec771a84214eb8d52a2c159cc00ee61c7b12
Tobias Loew <tobi@die-loew.de>
2022-07-09 10:28:11 +0200

    formatted with black

--- com/win32com/test/testmakepy.py
+++ com/win32com/test/testmakepy.py
@@ -12,21 +12,26 @@ from win32com.client import makepy, selecttlb, gencache
 import pythoncom
 import winerror
 
-def TestBuildAll(verbose = 1, bCreateEnums=False, bTypeHints=False):
+
+def TestBuildAll(verbose=1, bCreateEnums=False, bTypeHints=False):
     num = 0
     tlbInfos = selecttlb.EnumTlbs()
     for info in tlbInfos:
         if verbose:
             print("%s (%s)" % (info.desc, info.dll))
         try:
-            makepy.GenerateFromTypeLibSpec(info, None, None, None, None, 0, 1, bCreateEnums, bTypeHints)
-#          sys.stderr.write("Attr typeflags for coclass referenced object %s=%d (%d), typekind=%d\n" % (name, refAttr.wTypeFlags, refAttr.wTypeFlags & pythoncom.TYPEFLAG_FDUAL,refAttr.typekind))
+            makepy.GenerateFromTypeLibSpec(
+                info, None, None, None, None, 0, 1, bCreateEnums, bTypeHints
+            )
+            #          sys.stderr.write("Attr typeflags for coclass referenced object %s=%d (%d), typekind=%d\n" % (name, refAttr.wTypeFlags, refAttr.wTypeFlags & pythoncom.TYPEFLAG_FDUAL,refAttr.typekind))
             num += 1
         except pythoncom.com_error as details:
             # Ignore these 2 errors, as the are very common and can obscure
             # useful warnings.
-            if details.hresult not in [winerror.TYPE_E_CANTLOADLIBRARY,
-                              winerror.TYPE_E_LIBNOTREGISTERED]:
+            if details.hresult not in [
+                winerror.TYPE_E_CANTLOADLIBRARY,
+                winerror.TYPE_E_LIBNOTREGISTERED,
+            ]:
                 print("** COM error on", info.desc)
                 print(details)
         except KeyboardInterrupt:
@@ -44,7 +49,8 @@ def TestBuildAll(verbose = 1, bCreateEnums=False, bTypeHints=False):
                 makepy.GenerateChildFromTypeLibSpec(name, tinfo)
     return num
 
-def TestAll(verbose = 0):
+
+def TestAll(verbose=0):
     num = TestBuildAll(verbose)
     print("Generated and imported", num, "modules")
 
@@ -55,10 +61,14 @@ def TestAll(verbose = 0):
             num = TestBuildAll(verbose, False, True)
             print("Generated and imported", num, "modules with type-hints")
             num = TestBuildAll(verbose, True, True)
-            print("Generated and imported", num, "modules with type-hints and Python-Enums")
-        
+            print(
+                "Generated and imported",
+                num,
+                "modules with type-hints and Python-Enums",
+            )
 
     win32com.test.util.CheckClean()
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     TestAll("-q" not in sys.argv)
