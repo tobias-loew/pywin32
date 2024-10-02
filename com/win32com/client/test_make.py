aed77573a4eaabc790e5acd78669e0712097db1c
Löw, Tobias <Tobias.Loew@iqony.energy>
2024-10-02 16:27:10 +0200

    added files

---
+++ com/win32com/client/test_make.py
@@ -0,0 +1,8 @@
+import makepy
+#import EbsOpen 
+
+i=4
+makepy.main()
+#makepy.main(['-t ','-e', 'classes', '-r' , '-o', 'EbsOpen.py', 'D:\\loew\\proj\\ebsilon_17.git\\stage\\x64\Debug\bin\\EbsOpen.tlb'])
+
+
